9e0b26b4c72156ea713a2219e7152cad14059d10
MarlonWiss2212 <marlon.wisskirchen@outlook.de>
2024-02-08 15:50:59 +0100

    Projektdateien hinzufügen.

---
+++ Todo API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo_API.Models;
+
+namespace Todo_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+                
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasKey(pc => new { pc.Id});
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Todo API/Seed.cs
@@ -0,0 +1,34 @@
+﻿using Todo_API.Data;
+using Todo_API.Models;
+
+namespace Todo_API
+{
+    public class Seed(DataContext dataContext)
+    {
+        private readonly DataContext dataContext = dataContext;
+
+        public void SeedDataContext()
+        {
+            if(!dataContext.Todos.Any())
+            {
+                var todos = new List<Todo>()
+                {
+                    new Todo()
+                    {
+                        Title = "todo1",
+                        Description = "Description1",
+                        CreatedAt = DateTime.Now,
+                    },
+                    new Todo()
+                    {
+                        Title = "todo2",
+                        Description = "Description2",
+                        CreatedAt = DateTime.Now,
+                    }
+                };
+                dataContext.Todos.AddRange(todos);
+                dataContext.SaveChanges();
+            }
+        }
+    }
+}
